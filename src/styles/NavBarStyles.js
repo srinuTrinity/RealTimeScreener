dc5f74c3a1bf8273836a23a66b80a731187806fc
srinu879033 <srinu879033@gmail.com>
2021-07-28 13:29:41 +0530

    Modifications as per review

--- src/styles/NavBarStyles.js
+++ src/styles/NavBarStyles.js
@@ -5,12 +5,20 @@ const NavBarStyles = {
     borderColor: "#301b5c",
     // backgroundImage: "linear-gradient(to right,#84b7e0)",
     backgroundColor: "#634882",
+    "@media (max-width:768px)": {
+      paddingBottom: -10,
+    },
   },
   item: {
     marginRight: 16,
     fontWeight: 700,
     fontFamily: "Poppins",
     fontSize: 18,
+  },
+  leftAlign: {
+    "@media (min-width:992px)": {
+      marginLeft: "60%",
+    },
   },
 };
 export default NavBarStyles;
